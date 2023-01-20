1e342b04f432c100f145561a10d796e396dd8da8
mozammilkarim <mozammilkarim@gmail.com>
2023-01-20 22:07:52 +0530

    demo live

--- src/components/PrimaryButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import  "./button.css";
-const PrimaryButton = ({text="Primary"}) => {
-  return (
-    <div  className='primaryButton px-5 py-2 text-[#FAF8F6] bg-[#172339]   rounded-[6px]'>{text}</div>
-  )
-}
-
-export default PrimaryButton
--- src/components/SecondaryButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-const SecondaryButton = ({text="SecondaryButton"}) => {
-  return (
-    <div  className='text-center px-5 py-2 border-2 border-[#172339] bg-white text-[#172339] hover:text-[#FAF8F6] hover:bg-[#172339]   rounded-[6px]'>{text}</div>
-  )
-}
-
-export default SecondaryButton
